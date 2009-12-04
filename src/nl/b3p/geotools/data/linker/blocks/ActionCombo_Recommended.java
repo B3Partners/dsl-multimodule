6d70e334048ddd37e451ec2ba21953fbca832515
Gertjan Al <gertjanal@gmail.com>
2009-12-04 14:57:53 +0000

    Safe automatic typename rename

--- src/nl/b3p/geotools/data/linker/blocks/ActionCombo_Recommended.java
+++ src/nl/b3p/geotools/data/linker/blocks/ActionCombo_Recommended.java
@@ -11,22 +11,16 @@ public class ActionCombo_Recommended extends ActionCombo {
 
     public ActionCombo_Recommended() {
         // Trim values to 255 characters
-        Action action1 = new ActionFeature_Value_Substring_All(0, 255);
-        actionList.add(action1);
+        actionList.add(new ActionFeature_Value_Substring_All(0, 255));
 
         // Set typename to a safe value
-        Action action2 = new ActionFeatureType_Typename_Replace("([ ])", "_");
-        actionList.add(action2);
-
-        Action action3 = new ActionFeatureType_Typename_Replace("([^a-zA-Z0-9_])", "");
-        actionList.add(action3);
-
-        Action action4 = new ActionFeatureType_Typename_Case(false);
-        actionList.add(action4);
+        actionList.add(new ActionFeatureType_Typename_Replace("([ ])", "_"));
+        actionList.add(new ActionFeatureType_Typename_Replace("([^a-zA-Z0-9_])", ""));
+        actionList.add(new ActionFeatureType_Typename_Replace("^[0-9]", "_$0"));
+        actionList.add(new ActionFeatureType_Typename_Case(false));
 
         // Set EPSG:28992
-        Action action5 = new ActionFeatureType_Set_CRS("EPSG:28992");
-        actionList.add(action5);
+        actionList.add(new ActionFeatureType_Set_CRS("EPSG:28992"));
     }
 
 
