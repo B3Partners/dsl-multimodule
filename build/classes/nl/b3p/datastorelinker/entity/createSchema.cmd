ddd0584a2b04c1d1bf40eb76067423c730210e52
Erik van de Pol <erikvandepol@b3partners.nl>
2010-07-01 19:12:59 +0000

    build enzo eruit, ignored

--- build/classes/nl/b3p/datastorelinker/entity/createSchema.cmd
+++
@@ -1,1 +0,0 @@
-\dev_erik\DatastoreLinker\DataStoreLinkerGui\jaxb-ri-20090708\bin\schemagen.bat -cp C:\dev_erik\libs\HibernateEntityManager\trunk\ejb3-persistence.jar Process.java Inout.java InoutDatatype.java InoutType.java File.java Database.java DatabaseType.java
