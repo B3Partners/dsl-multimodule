454b08af45b88e834a724628461adb61ca8a0a24
Badr Ahmad <badrahmad6@gmail.com>
2016-05-19 14:18:48 +0200

    wachtwoord beheerder aanpassen

--- src/main/database/schema_export-postgres.sql
+++ src/main/database/schema_export-postgres.sql
@@ -443,7 +443,7 @@ ALTER TABLE ONLY qrtz_triggers
 
 -- insert default beheerder // beheerder account, password = beheerder
 insert into organization(id, name, upload_path) values (1, 'Beheerders', '/');
-insert into users(id, name, password, is_admin) values (1, 'beheerder', '6064aa54989438b77e4e605fe0405069f1159c51', true);
+insert into users(id, name, password, is_admin) values (1, 'beheerder', '1ZkPjF0ZNpQOXRr0TImwog%3D%3D', true);
 insert into organization_users(organization_id, users_id) values (1, 1);
 
 INSERT INTO qrtz_locks values('TRIGGER_ACCESS');
